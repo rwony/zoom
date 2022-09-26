63a4585c65a5712cb043e12f6f78da8c18005538
rwony <dev.rwlee@gmail.com>
2022-09-26 23:37:11 +0900

    set server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+import http from "http";
+import { Server } from "socket.io";
+import express from "express";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+httpServer.listen(3000, handleListen);
