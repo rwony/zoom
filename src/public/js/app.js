9236b9be64cfe51800e637277d9190240a20dc91
rwony <dev.rwlee@gmail.com>
2022-09-27 23:44:47 +0900

    complete

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,14 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -54,8 +59,6 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
-
 function handleMuteClick() {
   myStream
     .getAudioTracks()
@@ -85,9 +88,110 @@ function handleCameraClick() {
 }
 
 async function handleCameraChange() {
+  // 카메라를 변경하면 새로운 디바이스로 업데이트 된 video track을 받는다.
   await getMedia(camerasSelect.value);
+
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+
+    // Sender :  media stream track을 컨트롤 하게 해주는 역할
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.tack.kind === "video");
+
+    // 변경된 비디오 트랙으로 변경
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 mute.addEventListener("click", handleMuteClick);
 camera.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  // 방에 입장하면 실행되는 함수
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+socket.on("welcome", async () => {
+  // Peer A에서 실행됨 (누군가 들어왔다고 알림을 받는 브라우저)
+  // 이 곳에서 하는 일 : Create offer, setLocalDescription, Send offer to Peer B
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", async (offer) => {
+  // Peer B에서 실행됨
+  console.log("recieved the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("send the answer");
+});
+
+socket.on("answer", (answer) => {
+  console.log("recived the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+function handleIce(data) {
+  // 각 브라우저가 candidate들을 서로 주고 받음
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
+}
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  }); // Create peerConnection
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+
+  // 양쪽 브라우저에서 카메라와 마이크의 데이터 스트림을 받아서 그것들을 연결 안에 넣음
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
